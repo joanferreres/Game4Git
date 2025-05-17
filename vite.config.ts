a48293ea520cc0e59bea9e9e04ba979f47ce389a
joanferreres <joan.ferreres@students.salle.url.edu>
2025-05-17 19:48:22 +0200

    fix: add custom rollup resolver to fix Vercel deployment issues

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import customResolve from "./vite.resolve";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -19,6 +20,7 @@ export default defineConfig(({ mode }) => ({
     react(),
     mode === 'development' &&
     componentTagger(),
+    customResolve(),
   ].filter(Boolean),
   resolve: {
     alias: {
